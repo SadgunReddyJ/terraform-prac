2a1ce6ceb6207f452b202dc652a73937c2bb767f
SadgunReddyJ <javidisadgunreddy@gmail.com>
2024-10-09 12:40:24 +0530

    route tables

--- vpc.tf
+++ vpc.tf
@@ -43,3 +43,29 @@ resource "aws_internet_gateway" "crm-igw" {
     Name = "crm-internet-gateway"
   }
 }
+
+# crm public route table 
+
+resource "aws_route_table" "crm-public-rt" {
+  vpc_id = aws_vpc.crm-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.crm-igw.id
+  }
+
+  tags = {
+    Name = "crm-public-route"
+  }
+}
+
+# crm private route table
+
+
+resource "aws_route_table" "crm-private-rt" {
+  vpc_id = aws_vpc.crm-vpc.id
+
+  tags = {
+    Name = "crm-pubprivatelic-route"
+  }
+}
