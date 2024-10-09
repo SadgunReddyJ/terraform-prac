2225b59ac2a37dea6b19c04d46d816356b41dc12
SadgunReddyJ <javidisadgunreddy@gmail.com>
2024-10-09 14:52:48 +0530

    new server

---
+++ ec2.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "crm-web-server" {
+  ami           = "ami-0dee22c13ea7a9a67" # 
+  instance_type = "t2.micro"
+  subner_id = aws_subnet.cmr-web-subnet.id
+  key_name = "aws_login"
+  vpc_security_group_ids = aws_security_group.crm-web-sg.id
+  user_data = file("setup.sh")
+    tags = {
+    Name = "crm-web-server"
+  }
+}
